def4cc53d50648efae0e66659294ee2ea043a127
Prerit <32776569+prer1t@users.noreply.github.com>
2019-06-24 11:55:11 +0530

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,12 @@
+knn_scores = []
+for k in range(1,21):
+    knn_classifier = KNeighborsClassifier(n_neighbors = k)
+    knn_classifier.fit(X_train, y_train)
+    knn_scores.append(knn_classifier.score(X_test, y_test))
+plt.plot([k for k in range(1, 21)], knn_scores, color = 'red')
+for i in range(1,21):
+    plt.text(i, knn_scores[i-1], (i, knn_scores[i-1]))
+plt.xticks([])
+plt.xlabel('Number of Neighbors (K)')
+plt.ylabel('Scores')
+plt.title('K Neighbors Classifier scores for different K values')

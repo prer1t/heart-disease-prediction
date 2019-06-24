55e59844709914b70f7d1db914b08ced426bd155
Prerit <32776569+prer1t@users.noreply.github.com>
2019-06-24 11:53:20 +0530

    Create SVM.py

---
+++ SVM.py
@@ -0,0 +1,14 @@
+svc_scores = []
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+for i in range(len(kernels)):
+    svc_classifier = SVC(kernel = kernels[i])
+    svc_classifier.fit(X_train, y_train)
+    svc_scores.append(svc_classifier.score(X_test, y_test))
+    
+colors = rainbow(np.linspace(0, 1, len(kernels)))
+plt.bar(kernels, svc_scores, color = colors)
+for i in range(len(kernels)):
+    plt.text(i, svc_scores[i], svc_scores[i])
+plt.xlabel('Kernels')
+plt.ylabel('Scores')
+plt.title('scores in different kernels')
